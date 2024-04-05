59e4c04067193f8d55a60c64723243a98b95c761
Vinicius Barbosa <viniciusbarbosa934@gmail.com>
2024-04-05 16:30:38 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -43,5 +43,9 @@ public class Board {
 		
 	}
 
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 
 }
